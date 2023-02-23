c562b98b40b0e5aea1d2981988ced3dce03a03ed
RafaelMatos <rafael-matos@outlook.com>
2023-02-23 09:02:47 -0300

    feat(form):stalrd input and button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,14 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <input type="text" id="task" />
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para a sua tarefa"
+          />
 
           <label htmlFor="">durante</label>
-          <input type="text" id="minutesAmount" />
+          <MinutesAmountInput type="text" id="minutesAmount" placeholder="00" />
           <span>minutos.</span>
         </FormContainer>
         <CountdownContainer>
@@ -25,10 +32,10 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled={true}>
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
